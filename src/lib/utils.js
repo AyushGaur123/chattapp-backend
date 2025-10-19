0baf55daa82ec6daa51cbcd4c8e87f172b614822
AyushGaur123 <ayushgaur914@gmail.com>
2025-10-19 11:24:52 +0530

    update

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken"
 
 
 export const generateToken = (userId,res)=>{
-const token = jwt.sign({userId},process.env.JWT_SECRET)
+const token = jwt.sign({userId},process.env.JWT_SECRET,)
 
 
 res.cookie("jwt", token, {
     maxAge: 7*24*60*60*1000,
-    httpOnly: true,
+    // httpOnly: true,
     secure: false,       // in dev, must be false (since you're not on https)
     sameSite: "lax",     // or "none" if needed
 })
